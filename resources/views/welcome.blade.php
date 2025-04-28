bd7e368433e09213fc2ccaad9fc1705f7e98eae6
FedericoBattisti <fed.battisti@gmail.com>
2025-04-28 23:31:07 +0200

    inizializzato progetto, costruita navbar e footer, impostato css di base e rotta iniziale

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <div class="container my-3">
+        <div class="row">
+            <div class="col-md-6 mx-auto text-center">
+                <h1 class="titolo-custom">Benvenuto in MÀCRÌ</h1>
+            </div>
+        </div>
+    </div>
+</x-layout>
