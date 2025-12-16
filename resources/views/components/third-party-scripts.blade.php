6361bec4c4e8b7e627461d07d5d6f7d63e12e0f4
FedericoBattisti <fed.battisti@gmail.com>
2025-12-16 20:17:34 +0100

    s

--- resources/views/components/third-party-scripts.blade.php
+++ resources/views/components/third-party-scripts.blade.php
@@ -1,6 +1,10 @@
 <!-- Script di terze parti - Caricati condizionalmente -->
 
-@if(!request()->cookie('privacy_accepted'))
+@php
+    $privacyAccepted = isset($_COOKIE['privacy_accepted']) && $_COOKIE['privacy_accepted'] === 'true';
+@endphp
+
+@if(!$privacyAccepted)
     <!-- Script iubenda - Privacy Policy -->
     <script type="text/javascript" src="https://embeds.iubenda.com/widgets/1ac07740-367f-43b9-9592-d407902c1105.js"></script>
 @endif
@@ -12,8 +16,15 @@
 
         if (hasAccepted && hasAccepted.split('=')[1] === 'true') {
             // Rimuovi elementi iubenda dal DOM
-            const iubendaElements = document.querySelectorAll('.iubenda-embed, [class*="iubenda"], #iubenda-cs-banner');
-            iubendaElements.forEach(element => element.remove());
+            const iubendaElements = document.querySelectorAll('.iubenda-embed, [class*="iubenda"], #iubenda-cs-banner, .iubenda-cs-container');
+            iubendaElements.forEach(element => {
+                element.style.display = 'none';
+                element.remove();
+            });
+
+            // Blocca anche eventuali script iubenda caricati
+            const iubendaScripts = document.querySelectorAll('script[src*="iubenda"]');
+            iubendaScripts.forEach(script => script.remove());
         }
     });
 </script>
