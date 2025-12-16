846175c86fb10dc4aacf9448a5da2af8bd70ae65
FedericoBattisti <fed.battisti@gmail.com>
2025-12-16 17:49:21 +0100

    contatto

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\SendGridApiTransport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
         if (app()->environment('production')) {
             URL::forceScheme('https');
         }
+
+        Mail::extend('sendgrid_api', function () {
+            return new SendGridApiTransport(config('services.sendgrid.api_key'));
+        });
     }
 }
